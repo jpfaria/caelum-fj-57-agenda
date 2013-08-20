e34e6cc533b02921cba14b38c725497e84cba0f7
jpfaria <jpfaria@gmail.com>
2013-08-20 18:29:23 -0300

    .

--- src/br/com/caelum/cadastro/helper/FormularioHelper.java
+++ src/br/com/caelum/cadastro/helper/FormularioHelper.java
@@ -73,7 +73,7 @@ public class FormularioHelper {
 		} else {
 			fotoBitmap = BitmapFactory.decodeResource(activity.getResources(), R.drawable.ic_no_image);
 		}
-		fotoBitmap = Bitmap.createScaledBitmap(fotoBitmap, 100, 100, true);
+		fotoBitmap = Bitmap.createScaledBitmap(fotoBitmap, 160, 160, true);
 		botaoImagem.setImageBitmap(fotoBitmap);
 	}
 	
